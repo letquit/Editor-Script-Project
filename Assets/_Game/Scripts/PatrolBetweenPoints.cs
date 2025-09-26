1f103270b960ec3894748ce66a60dd54283e8040
letquit <1446444589@qq.com>
2025-09-26 11:33:42 +0800

    PatrolLIne
    Gizmos绘制点与连接路径

---
+++ Assets/_Game/Scripts/PatrolBetweenPoints.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+public class PatrolBetweenPoints : MonoBehaviour
+{
+    [SerializeField] 
+    private Transform[] _patrolPoints;
+
+    private void OnDrawGizmos()
+    {
+        DrawPoints();
+        DrawPaths();
+    }
+
+    private void DrawPaths()
+    {
+        Gizmos.color = Color.yellow;
+        for (int i = 0; i < _patrolPoints.Length - 1; i++)
+        {
+            if (_patrolPoints[i] != null && _patrolPoints[i + 1] != null)
+            {
+                Vector3 thisPoint = _patrolPoints[i].position;
+                Vector3 nextPoint = _patrolPoints[i + 1].position;
+                Gizmos.DrawLine(thisPoint, nextPoint);   
+            }
+        }
+    }
+
+    private void DrawPoints()
+    {
+        Gizmos.color = Color.yellow;
+        foreach (Transform point in _patrolPoints)
+        {
+            if (point != null)
+                Gizmos.DrawSphere(point.position, 0.25f);
+        }
+    }
+}
