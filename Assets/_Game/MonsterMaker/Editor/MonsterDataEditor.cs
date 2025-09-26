225e86695ae6f7465c2ab16ab3b373c89618d262
letquit <1446444589@qq.com>
2025-09-26 14:53:50 +0800

    Display Bar
    进度条

--- Assets/_Game/MonsterMaker/Editor/MonsterDataEditor.cs
+++ Assets/_Game/MonsterMaker/Editor/MonsterDataEditor.cs
@@ -9,9 +9,19 @@ public class MonsterDataEditor : Editor
         MonsterData data = (MonsterData)target;
         EditorGUILayout.LabelField(data.Name.ToUpper(), EditorStyles.boldLabel);
         EditorGUILayout.Space(10);
+
+        float difficulty = data.Health + data.Damage + data.Speed; 
+        ProgressBar(difficulty / 100, "Difficulty");
         
         base.OnInspectorGUI();
         
         EditorGUILayout.LabelField("-----------");
+    }
+
+    private void ProgressBar(float value, string label)
+    {
+        Rect rect = GUILayoutUtility.GetRect(18, 18, "TextField");
+        EditorGUI.ProgressBar(rect, value, label);
+        EditorGUILayout.Space(10);
     }
 }
