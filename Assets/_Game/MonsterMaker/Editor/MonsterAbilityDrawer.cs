30343204105c6f1cbc353f8b3d0b1a8fbaafd580
letquit <1446444589@qq.com>
2025-09-27 23:42:37 +0800

    Drawing Properties
    绘图属性

--- Assets/_Game/MonsterMaker/Editor/MonsterAbilityDrawer.cs
+++ Assets/_Game/MonsterMaker/Editor/MonsterAbilityDrawer.cs
@@ -11,13 +11,45 @@ public class MonsterAbilityDrawer : PropertyDrawer
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         EditorGUI.BeginProperty(position, label, property);
+        
+        // fill our properties
+        _name = property.FindPropertyRelative("_name");
+        _damage = property.FindPropertyRelative("_damage");
+        _element = property.FindPropertyRelative("_element");
 
         Rect foldOutBox = new Rect(position.min.x, position.min.y, position.size.x, EditorGUIUtility.singleLineHeight);
         property.isExpanded = EditorGUI.Foldout(foldOutBox, property.isExpanded, label);
-        
-        // draw our properties
+
+        if (property.isExpanded)
+        {
+            // draw our properties
+            DrawNameProperty(position);
+            DrawDamageProperty(position);
+            DrawElementProperty(position);
+        }
         
         EditorGUI.EndProperty();
+    }
+
+    private void DrawNameProperty(Rect position)
+    {
+        float xPos = position.min.x;
+        float yPos = position.min.y + EditorGUIUtility.singleLineHeight;
+        float width = position.size.x;
+        float height = EditorGUIUtility.singleLineHeight;
+        
+        Rect drawArea = new Rect(xPos, yPos, width, height);
+        EditorGUI.PropertyField(drawArea, _name, new GUIContent("Name"));
+    }
+
+    private void DrawDamageProperty(Rect position)
+    {
+        
+    }
+
+    private void DrawElementProperty(Rect position)
+    {
+        
     }
 
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
